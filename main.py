c8ea03bb7234422a6655c7ebc293140581fb0f9a
irainy <3074117902@qq.com>
2026-05-09 16:45:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ get_current_dayofweek = lambda action: (
 )
 
 
-SLEEPTIME = 0.5  # 每次抢座的间隔
+SLEEPTIME = 3  # 每次抢座的间隔
 ENDTIME = "20:01:00"  # 根据学校的预约座位时间+1min即可
 
 ENABLE_SLIDER = True  # 是否有滑块验证
-MAX_ATTEMPT = 150  # 最大尝试次数
+MAX_ATTEMPT = 60  # 最大尝试次数
 RESERVE_NEXT_DAY = False  # 预约明天而不是今天的
 
 
